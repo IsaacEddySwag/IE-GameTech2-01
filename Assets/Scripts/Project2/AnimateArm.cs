99f99243d90cb20b98f0e2987d1dd9ac22fb9422
MTL\ieddy <isaac.eddy@mymail.champlain.edu>
2023-10-19 00:17:29 -0400

    Began work cleaning up game build

--- Assets/Scripts/Project2/AnimateArm.cs
+++ Assets/Scripts/Project2/AnimateArm.cs
@@ -8,19 +8,25 @@ public class AnimateArm : MonoBehaviour
     private ThirdPersonCharacterController characterController;
     public void Awake()
     {
+        //Gets the animator of the object the scripts attached too and sets it to a variable
         animator = GetComponent<Animator>();
+        //Gets the character controller from the player character and sets it to a variable.
         characterController = GameObject.Find("3rd Person Player").GetComponent<ThirdPersonCharacterController>();
     }
+
+    //Function sets the arm swing animator bool to be equal to the bool passed
     public void armMove(bool swing)
     {
         animator.SetBool("SwingR", swing);
     }
 
+    //Function sets the arm swing animator bool to be equal to false
     public void stopMove()
     {
         animator.SetBool("SwingR", false);
     }
 
+    //Function refrences the throwball function on the character controller script. Function spawns a moving snowball.
     public void Throw()
     {
         characterController.ThrowBall();
--- Assets/Scripts/Project2/HealthBar.cs
+++ Assets/Scripts/Project2/HealthBar.cs
@@ -6,35 +6,46 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider;
+    private Slider slider;
     public Gradient gradient;
     public Image fill;
     private Animator anim;
 
+    //Gets the slider and animator of the object script is attached too and sets them to variables.
     private void Start()
     {
         slider = GetComponent<Slider>();
         anim = GetComponent<Animator>();
     }
 
+    //Sets the health bar slider to be equal to be equal to max health and updates the bar accordingly.
     public void SetMaxHealth(float maxHealth)
     {
+        //Sets the value of the slider to be equal to the passed variable maxhealth
         slider.maxValue = maxHealth;
+        //Sets the value of the slider equal to the value input of max health
         slider.value = maxHealth;
+        //Changes the gradient color based on the position of the health bar slider
         fill.color = gradient.Evaluate(1f);
     }
 
+    //Updates the player health bar to be equal to the players current health
     public void SetHealth(float health)
     {
+        //Sets the value of the slider equal to the value input of health
         slider.value = health;
+
+        //Changes the gradient color based on the position of the health bar slider
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
+    //Function sets bool to true, this variable in the animator so that the healthbar can fade in
     public void FadeIn()
     {
         anim.SetBool("HealthFade", true);
     }
 
+    //Function sets bool to true, this variable in the animator so that the healthbar can't fade in anymore
     public void FadeOut()
     {
         anim.SetBool("HealthFade", false);
